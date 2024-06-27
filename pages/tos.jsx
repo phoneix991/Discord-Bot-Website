79e4d9345b035cd971dcfb7dcdbf984acb3b8b63
phoneix991 <profatih009@gmail.com>
2024-06-28 02:58:30 +0300

    Türkçe tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,20 +1,20 @@
 export default function Tos() {
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+     <h2 className="mt-4 text-white font-bold text-3xl">Hizmet Şartları</h2>
+      <p className="text-sm text-white/30 mb-1">Son Güncelleme Tarihi: 28/06/2024 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Her discord sunucusu MusicMaker botunu ücretsiz olarak kullanabilir, ücret karşılığında satışı veya pazarlaması kesinlikle yasaktır. (Premium sürüm hariç).</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ Açık kaynak kodlarını diğer sunuculara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
 <p>⊳ Açık kaynak kodlarını diğer platformlar aracılığıyla satamaz veya pazarlayamazsınız.</p>
 <div className="py-3"></div>
 <p>⊳ Botu kullanırken trol amaçlı diğer kullanıcılara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot at certain intervals.</p>
+<p>⊳ Belirli discord bot komutlarını kullanabilmek için belirli aralıklarla MusicMaker botuna oy vermeniz gerekmektedir.</p>
 </div>
     </div>
   );

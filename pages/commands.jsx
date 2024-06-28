60a8f1603d91ee1243022701950e3ca7218158c4
phoneix991 <profatih009@gmail.com>
2024-06-28 12:13:42 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Commands() {
 const commands = [
-  "/autoplay",
+  "/phoneixpanel (Çok İşlevli Yönetim Paneli)",
   "/back",
   "/statistic",
   "/channel",
